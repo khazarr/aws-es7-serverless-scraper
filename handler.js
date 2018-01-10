67d540f8f9c0a329c09b8914d39ac9873e95a4ed
Karol <swieca.karol@gmail.com>
2018-01-10 21:39:50 +0100

    cleanup

--- handler.js
+++ handler.js
@@ -1,4 +1,4 @@
-//mock plugin structure
+//---- mock plugin structure--------------------------
 const pluginData = [
   {
     name: 'disable-comments',
@@ -21,35 +21,38 @@ const pluginData = [
     url: 'https://pl.wordpress.org/plugins/wp-hide-post/'
   },
 ]
+//--------------------------------------------
+
 
 const { getCurrentWPVer, getCurrentPluginVer } = require('./helpers');
 const request = require('axios');
-
+const pluginsToday, wpToday, pluginsYesterday, wpYesterday;
 
 export const hello = async (event, context, callback) => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify({
-      message: `Go Serverless v1.0! ${(await message({ time: 1, copy: 'Your function executed successfully!'}))}`,
-    }),
   };
 
-  let plug = await getAllPluginsVersion(pluginData)
-  response.plugins = plug
+  pluginsToday = await getAllPluginsVersion(pluginData)
+  response.plugins = pluginsToday
 
-  let wpVer = await getWpVer()
-  response.wp = wpVer;
+  wpToday = await getWpVer()
+  response.wp = wpToday;
 
+  //get yesterday db data
 
+  //compare data
+
+  //if data differ SNS
+
+  //delete old & save new data
 
   callback(null, response);
 };
 
-const message = ({ time, ...rest }) => new Promise((resolve, reject) =>
-  setTimeout(() => {
-    resolve(`${rest.copy} (with a delay)`);
-  }, time * 1000)
-);
+
+
+
 
 async function getWpVer() {
   const wpPage = await request('https://wordpress.org/download/')
